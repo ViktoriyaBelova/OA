c04a9ceb7ca6cb09bf3ed0a7347b42324bbd28d7
vikabelova <belova.viktoriya@gmail.com>
2025-03-16 15:22:29 +0700

    Initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,25 +1,19 @@
 package ru.netology.service;
 
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int expected = 100;
-        int actual = service.remain(amount);
-        assertEquals(actual, expected);
-    }
+    public void Remain() {
+        final CashbackHackService cashbackHackService = new CashbackHackService();
+        final int amount = 999;
 
-    @Test
-    public void testRemainWithExactBoundary() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
-        int expected = 0;
-        int actual = service.remain(amount);
+        final int actual = cashbackHackService.remain(amount);
+        final int expected = 999;
+
         assertEquals(actual, expected);
     }
 }
